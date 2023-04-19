d109ee78f43a5f59c50cc163338f25448aff3c6f
diplak <tamangdeepluv@gmail.com>
2023-04-20 04:18:35 +0545

    first commit

---
+++ home/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=300)
+    email = models.EmailField(max_length=300)
+    subject = models.TextField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Services(models.Model):
+    title = models.CharField(max_length=300)
+    logo = models.CharField(max_length=300)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=300)
+    post = models.CharField(max_length=300)
+    image = models.CharField(max_length=300)
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Information(models.Model):
+    address1 = models.CharField(max_length=200)
+    address2 = models.CharField(max_length=300)
+    phone = models.CharField(max_length=40)
+    email = models.EmailField(max_length=500)
+    time = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.address1
---
+++ home/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from .models import *
+# Create your views here.
+
+
+def home(request):
+    views={}
+    views['services'] = Services.objects.all
+    return render(request, 'index.html',views)
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def price(request):
+    return render(request, 'price.html')
+
