602d3e0584130217de90b95b9eba7dc44d0bea58
diplak <tamangdeepluv@gmail.com>
2023-05-14 09:37:17 +0545

    model

--- home/models.py
+++ home/models.py
@@ -67,4 +67,12 @@ class Post(models.Model):
     location_name= models.CharField(max_length=300)
 
     def __str__(self):
-        return self.name
+
+      return self.name
+
+
+class UploadImage(models.Model):
+    name= models.CharField(max_length=300)
+    image=models.ImageField(upload_to='media')
+    date= models.DateTimeField(auto_now_add=True)
+
